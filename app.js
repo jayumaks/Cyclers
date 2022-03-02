3d78ce03a018d87b21428f7d543a32ce988dc8fe
jayumaks <jayumaks@gmail.com>
2022-03-02 04:50:47 +0100

    fit hand

--- app.js
+++ app.js
@@ -35,4 +35,16 @@ function showDivs(n) {
 }
 
 
+let bar1 = document.querySelector(".bar1");
+let bar2 = document.querySelector(".bar2");
+let bar3 = document.querySelector(".bar3");
+let menubar = document.querySelector(".menubar");
+let links = document.querySelector(".nav-links-div");
+
+menubar.addEventListener("click", function () {
+  bar1.classList.toggle("barclose1");
+  bar2.classList.toggle("barclose2");
+  bar3.classList.toggle("barclose3");
+  links.classList.toggle("newlinks");
+});
 
