d0a99f2e791fdaab16d0dec424802a28a83f7f00
jayumaks <jayumaks@gmail.com>
2022-03-26 12:33:24 +0100

    gt

--- contact_us/contact.js
+++ contact_us/contact.js
@@ -3,4 +3,4 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.name.value = '';
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
-  });
+});
