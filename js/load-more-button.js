dd6c84d6f876a6fdd81b6686e80cfe1fce20c305
jayumaks <jayumaks@gmail.com>
2022-04-12 02:17:07 +0100

    gt

--- js/load-more-button.js
+++ js/load-more-button.js
@@ -1,13 +1,13 @@
- $(document).ready(function() {
-     $(".moreBox").slice(0, 2).show();
-     if ($(".blogBox:hidden").length != 0) {
-         $("#loadMore").show();
-     }
-     $("#loadMore").on('click', function(e) {
-         e.preventDefault();
-         $(".moreBox:hidden").slice(0, 7).slideDown();
-         if ($(".moreBox:hidden").length == 0) {
-             $("#loadMore").fadeOut('slow');
-         }
-     });
- });
+$(document).ready(function() {
+    $(".moreBox").slice(0, 2).show();
+    if ($(".blogBox:hidden").length != 0) {
+        $("#loadMore").show();
+    }
+    $("#loadMore").on('click', function(e) {
+        e.preventDefault();
+        $(".moreBox:hidden").slice(0, 7).slideDown();
+        if ($(".moreBox:hidden").length == 0) {
+            $("#loadMore").fadeOut('slow');
+        }
+    });
+});
