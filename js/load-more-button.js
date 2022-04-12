931d2ed78fbf2294a35df5b596b0090db8990b81
jayumaks <jayumaks@gmail.com>
2022-04-12 03:36:51 +0100

    gt

--- js/load-more-button.js
+++ js/load-more-button.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     }
     $("#loadMore").on('click', function(e) {
         e.preventDefault();
-        $(".moreBox:hidden").slice(0, 7).slideDown();
+        $(".moreBox:hidden").slice(0, 9).slideDown();
         if ($(".moreBox:hidden").length == 0) {
             $("#loadMore").fadeOut('slow');
         }
