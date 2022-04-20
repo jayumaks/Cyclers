c126ac5cadd7cebd7006ed8472f12c75592a431b
jayumaks <jayumaks@gmail.com>
2022-04-20 12:48:31 +0100

    gt

--- js/scroll.js
+++ js/scroll.js
@@ -49,43 +49,40 @@
 const scrollElements = document.querySelectorAll(".js-scroll");
 
 const elementInView = (el, dividend = 1) => {
-  const elementTop = el.getBoundingClientRect().top;
+    const elementTop = el.getBoundingClientRect().top;
 
-  return (
-    elementTop <=
-    (window.innerHeight || document.documentElement.clientHeight) / dividend
-  );
+    return (
+        elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
 };
 
 const elementOutofView = (el) => {
-  const elementTop = el.getBoundingClientRect().top;
+    const elementTop = el.getBoundingClientRect().top;
 
-  return (
-    elementTop > (window.innerHeight || document.documentElement.clientHeight)
-  );
+    return (
+        elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
 };
 
 const displayScrollElement = (element) => {
-  element.classList.add("scrolled");
+    element.classList.add("scrolled");
 };
 
 const hideScrollElement = (element) => {
-  element.classList.remove("scrolled");
+    element.classList.remove("scrolled");
 };
 
 const handleScrollAnimation = () => {
-  scrollElements.forEach((el) => {
-    if (elementInView(el, 1.25)) {
-      displayScrollElement(el);
-    } else if (elementOutofView(el)) {
-      hideScrollElement(el);
-    }
-  });
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 1.25)) {
+            displayScrollElement(el);
+        } else if (elementOutofView(el)) {
+            hideScrollElement(el);
+        }
+    });
 };
 
 window.addEventListener("scroll", () => {
-  handleScrollAnimation();
+    handleScrollAnimation();
 });
-
-
-
